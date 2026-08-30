91c0d4a8d0faef49efb4a73f788b55da63f2782e
RayyanKhan4004 <rk4004104@gmail.com>
2026-08-31 00:14:58 +0500

    fix build

--- features/marketing/types/index.ts
+++ features/marketing/types/index.ts
@@ -1,5 +1,5 @@
 export type { FooterColumn, FooterLink } from "./footer";
 export type { FoldingBoxFrame } from "./folding-box";
 export type { SectionHeaderProps } from "./home-sections";
-export type { Industry } from "./industries";
+export type { Industry, IndustryApiResponse } from "./industries";
 export type { PackagingStyle } from "./packaging-styles";
